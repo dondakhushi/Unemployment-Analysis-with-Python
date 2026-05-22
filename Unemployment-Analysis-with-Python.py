1b0da15ba6efe2a48b62ee66d82d5f2b40198513
Khushi Donda <khushidonda02@gmail.com>
2026-05-22 12:44:29 +0000

    Data Cleaning

--- Unemployment-Analysis-with-Python.py
+++ Unemployment-Analysis-with-Python.py
@@ -24,3 +24,16 @@ print(unemployment_df.describe())
 
 # Column names
 print(unemployment_df.columns)
+
+# Remove Extra Spaces from Column Names
+unemployment_df.columns = unemployment_df.columns.str.strip()
+
+# Check for missing values
+print(unemployment_df.isnull().sum())
+
+# Remove Missing Values
+unemployment_df.dropna(inplace=True)
+
+# Convert Date Column into Datetime Format
+unemployment_df['Date'] = pd.to_datetime(unemployment_df['Date'])
+
