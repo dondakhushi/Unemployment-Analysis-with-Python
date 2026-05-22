b5ba9f68bb7af9d0fc65b8da6d7cfcd2463528d7
Khushi Donda <khushidonda02@gmail.com>
2026-05-22 07:58:33 +0000

    Initial project setup with virtual environment and dependencies

---
+++ Unemployment-Analysis-with-Python.py
@@ -0,0 +1,12 @@
+# Data handling
+import pandas as pd
+import numpy as np
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
